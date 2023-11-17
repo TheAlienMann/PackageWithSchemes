12cb0059953959724a43cbc5a315275bb6424997
Mohamad Rahmani <rahmanipour.mohammad@gmail.com>
2023-11-17 20:55:49 +0330

    Make scheme for the tests + xctestplan for it + supported platforms to iOS 15 and macOS 10_15

--- Package.swift
+++ Package.swift
@@ -4,20 +4,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "PackageWithSchemes",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "PackageWithSchemes",
-            targets: ["PackageWithSchemes"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "PackageWithSchemes"),
-        .testTarget(
-            name: "PackageWithSchemesTests",
-            dependencies: ["PackageWithSchemes"]),
-    ]
+  name: "PackageWithSchemes",
+  platforms: [.iOS(.v15), .macOS(.v10_15)],
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "PackageWithSchemes",
+      targets: ["PackageWithSchemes"]),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "PackageWithSchemes"),
+    .testTarget(
+      name: "PackageWithSchemesTests",
+      dependencies: ["PackageWithSchemes"]),
+  ]
 )
